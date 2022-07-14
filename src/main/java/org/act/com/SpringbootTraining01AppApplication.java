af400ed378125c9c5f81acc62cddd3fb7075feab
Getu <getu1amu@gimail.com>
2022-07-14 11:39:26 +0300

    springboot-training01-app

---
+++ src/main/java/org/act/com/SpringbootTraining01AppApplication.java
@@ -0,0 +1,13 @@
+package org.act.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTraining01AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTraining01AppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/act/com/SpringbootTraining01AppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.act.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTraining01AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
