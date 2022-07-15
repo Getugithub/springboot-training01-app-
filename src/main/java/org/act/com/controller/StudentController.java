794a99c86d2aa65fca7446f31b44c5f06232510d
Getu <getu1amu@gimail.com>
2022-07-15 09:00:58 +0300

    REST API

--- src/main/java/org/act/com/controller/StudentController.java
+++ src/main/java/org/act/com/controller/StudentController.java
@@ -2,7 +2,12 @@ package org.act.com.controller;
 
 import org.act.com.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -11,5 +16,26 @@ public class StudentController {
     public Student getstudent(){
         return new Student("Getu","Kebebe");
     }
+    @GetMapping("/students")
+    public List<Student> getstudents(){
+        List<Student>students = new ArrayList<>();
+        students.add(new Student("getu","kebebe"));
+        students.add(new Student("jo","yoni"));
+        students.add(new Student("abi","Ja"));
+        students.add(new Student("yam","sol"));
+        return students;
+    }
 
+    @GetMapping("{firstname}/{lastname}")
+    public Student studentpathvariable(
+            @PathVariable("firstname") String firstname,
+            @PathVariable("lastname") String lastname){
+        return new Student(firstname,lastname);
+    }
+@GetMapping("student/query")
+    public Student studentQueryparm(
+            @RequestParam(name ="firstname")String firstname,
+            @RequestParam(name="lastname")String lastname){
+        return new Student(firstname,lastname);
 }
+ }
